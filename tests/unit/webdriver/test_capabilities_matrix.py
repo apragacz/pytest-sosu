70bb42a5bf3f49c4a9b9229a0003718cb0f4a0c7
Andrzej Pragacz <apragacz@o2.pl>
2022-01-18 16:02:35 +0100

    Add capabilities matrix unit tests

---
+++ tests/unit/webdriver/test_capabilities_matrix.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pytest_sosu.webdriver import Browser, Capabilities, CapabilitiesMatrix, Platform
+
+
+def test_iter_capabilities():
+    linux = Platform("Linux")
+    win_10 = Platform("Windows", "10")
+    chrome_97 = Browser("chrome", "97")
+    ff_96 = Browser("firefox", "96")
+    cm = CapabilitiesMatrix(
+        platforms=[linux, win_10],
+        browsers=[chrome_97, ff_96],
+    )
+    assert set(cm.iter_capabilities()) == {
+        Capabilities(platform=linux, browser=chrome_97),
+        Capabilities(platform=linux, browser=ff_96),
+        Capabilities(platform=win_10, browser=chrome_97),
+        Capabilities(platform=win_10, browser=ff_96),
+    }
