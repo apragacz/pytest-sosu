40a392170e23de173226cc9ccff0e396c5e927e4
Andrzej Pragacz <apragacz@o2.pl>
2022-01-19 12:00:38 +0100

    Fix pylint issue

--- tests/unit/webdriver/test_capabilities_matrix.py
+++ tests/unit/webdriver/test_capabilities_matrix.py
@@ -1,5 +1,3 @@
-import pytest
-
 from pytest_sosu.webdriver import Browser, Capabilities, CapabilitiesMatrix, Platform
 
 
@@ -8,11 +6,11 @@ def test_iter_capabilities():
     win_10 = Platform("Windows", "10")
     chrome_97 = Browser("chrome", "97")
     ff_96 = Browser("firefox", "96")
-    cm = CapabilitiesMatrix(
+    caps_matrix = CapabilitiesMatrix(
         platforms=[linux, win_10],
         browsers=[chrome_97, ff_96],
     )
-    assert set(cm.iter_capabilities()) == {
+    assert set(caps_matrix.iter_capabilities()) == {
         Capabilities(platform=linux, browser=chrome_97),
         Capabilities(platform=linux, browser=ff_96),
         Capabilities(platform=win_10, browser=chrome_97),
