8a29717eb43b792bc91eb7a29e8bd1f52277ba37
Andrzej Pragacz <apragacz@o2.pl>
2022-11-17 00:33:06 +0100

    Fix region choosing

--- pytest_sosu/config.py
+++ pytest_sosu/config.py
@@ -5,7 +5,10 @@ from typing import Optional
 
 from _pytest.config import UsageError
 
+from pytest_sosu.logging import get_struct_logger
 from pytest_sosu.webdriver import WebDriverUrlData
+
+logger = get_struct_logger(__name__)
 
 
 @dataclass
@@ -25,6 +28,7 @@ class SosuConfig:
 
 
 def build_sosu_config(args: argparse.Namespace, env: os._Environ) -> SosuConfig:
+    logger.debug("build_sosu_config", args=args, env=env)
     username = args.sosu_username or env.get("SAUCE_USERNAME")
     access_key = args.sosu_access_key or env.get("SAUCE_ACCESS_KEY")
 
@@ -34,11 +38,10 @@ def build_sosu_config(args: argparse.Namespace, env: os._Environ) -> SosuConfig:
     if not access_key:
         raise UsageError("--sosu-access-key or SAUCE_ACCESS_KEY are not provided")
 
-    region = args.sosu_region or env.get("SAUCE_REGION")
+    region: Optional[str] = args.sosu_region or env.get("SAUCE_REGION")
     webdriver_url = args.sosu_webdriver_url or env.get("SAUCE_WEBDRIVER_URL")
 
     if not webdriver_url:
-        region = "us-west-1"
         host = get_host_by_region(region)
         webdriver_url_data = WebDriverUrlData(host=host)
     else:
@@ -55,7 +58,7 @@ def build_sosu_config(args: argparse.Namespace, env: os._Environ) -> SosuConfig:
     )
 
 
-def get_host_by_region(region: str) -> str:
+def get_host_by_region(region: Optional[str]) -> str:
     if not region or region == "global":
         return "ondemand.saucelabs.com"
     host_region = REGION_MAP.get(region, region)
