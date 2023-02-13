7ef6655d3c2f81c6f320d5745656318326a9b121
Andrzej Pragacz <apragacz@o2.pl>
2023-02-13 23:20:09 +0100

    Do not force to use structlog

--- pytest_sosu/logging.py
+++ pytest_sosu/logging.py
@@ -1,34 +1,125 @@
 import logging
-
-import structlog
-from structlog.stdlib import filter_by_level, render_to_log_kwargs
-from structlog.types import EventDict
+from typing import Callable, Mapping, Optional, Type
 
 
 def get_struct_logger(name):
-    return _get_stdlib_struct_logger(name)
+    return LazyStructLogger(struct_logger_factory, name)
 
 
-def _get_stdlib_struct_logger(name):
-    logger = structlog.wrap_logger(
-        logging.getLogger(name),
-        processors=[
-            filter_by_level,
-            render_event_dict_into_msg,
-            render_to_log_kwargs,
-        ],
-    )
-    return logger
+class StructLogger:
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._func_dict = {
+            logging.DEBUG: self.debug,
+            logging.INFO: self.info,
+            logging.WARNING: self.warning,
+            logging.ERROR: self.error,
+            logging.CRITICAL: self.critical,
+        }
+
+    def debug(self, msg, **kwargs):
+        raise NotImplementedError()
+
+    def info(self, msg, **kwargs):
+        raise NotImplementedError()
+
+    def warning(self, msg, **kwargs):
+        raise NotImplementedError()
+
+    def error(self, msg, **kwargs):
+        raise NotImplementedError()
+
+    def critical(self, msg, **kwargs):
+        raise NotImplementedError()
+
+    def log(self, level, msg, **kwargs):
+        func = self._func_dict.get(level, self.debug)
+        return func(msg, **kwargs)
+
+    def exception(self, msg, **kwargs):
+        raise NotImplementedError()
 
 
-def render_event_dict_into_msg(
-    logger: logging.Logger, method_name: str, event_dict: EventDict
-):
-    event = event_dict.pop("event")
-    msg = event + "     " + " ".join(_iter_kv_strings(event_dict))
-    return {"event": msg}
+class StdlibStructLogger(StructLogger):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self._logger = logging.getLogger(name)
+
+    def debug(self, msg, **kwargs):
+        return self._logger.debug(render_full_message(msg, kwargs))
+
+    def info(self, msg, **kwargs):
+        return self._logger.info(render_full_message(msg, kwargs))
+
+    def warning(self, msg, **kwargs):
+        return self._logger.warning(render_full_message(msg, kwargs))
+
+    def error(self, msg, **kwargs):
+        return self._logger.error(render_full_message(msg, kwargs))
+
+    def critical(self, msg, **kwargs):
+        return self._logger.critical(render_full_message(msg, kwargs))
+
+    def exception(self, msg, **kwargs):
+        return self._logger.exception(render_full_message(msg, kwargs))
 
 
-def _iter_kv_strings(event_dict: EventDict):
-    for name, value in event_dict.items():
-        yield f"{name}={value}"
+class LazyStructLogger(StructLogger):
+    def __init__(
+        self,
+        factory: Callable[[str], StructLogger],
+        name: str,
+    ) -> None:
+        super().__init__(name)
+        self._factory = factory
+        self._logger: Optional[StructLogger] = None
+
+    @property
+    def logger(self) -> StructLogger:
+        if self._logger is not None:
+            return self._logger
+        self._logger = self._factory(self._name)
+        return self._logger
+
+    def debug(self, msg, **kwargs):
+        return self.logger.debug(msg, **kwargs)
+
+    def info(self, msg, **kwargs):
+        return self.logger.info(msg, **kwargs)
+
+    def warning(self, msg, **kwargs):
+        return self.logger.warning(msg, **kwargs)
+
+    def error(self, msg, **kwargs):
+        return self.logger.error(msg, **kwargs)
+
+    def critical(self, msg, **kwargs):
+        return self.logger.critical(msg, **kwargs)
+
+    def exception(self, msg, **kwargs):
+        return self.logger.exception(msg, **kwargs)
+
+
+class StructLoggerFactory:
+    def __init__(self, struct_logger_cls: Type[StructLogger]) -> None:
+        self._struct_logger_cls = struct_logger_cls
+
+    def __call__(self, name: str) -> StructLogger:
+        return self._struct_logger_cls(name)
+
+    def set_struct_logger_class(self, struct_logger_cls: Type[StructLogger]) -> None:
+        self._struct_logger_cls = struct_logger_cls
+
+
+struct_logger_factory = StructLoggerFactory(StdlibStructLogger)
+set_struct_logger_class = struct_logger_factory.set_struct_logger_class
+
+
+def render_full_message(msg: str, data: Mapping) -> str:
+    full_msg = msg + "     " + " ".join(_iter_kv_strings(data))
+    return full_msg
+
+
+def _iter_kv_strings(data: Mapping):
+    for name, value in data.items():
+        yield f"{name}={value!r}"
